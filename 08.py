7d7a807944d32e6a7d34f63977729853f311b48c
Ondra <tethal@email.cz>
2023-12-08 09:37:08 +0100

    08

---
+++ 08.py
@@ -0,0 +1,26 @@
+import math
+import re
+
+RE = re.compile(r'(\w+) = \((\w+), (\w+)\)')
+
+
+nodes = {}
+with open('08a.txt', 'rt') as f:
+    ins = f.readline().strip()
+    f.readline()
+    for line in f:
+        m = re.search(RE, line)
+        n, l, r = m.groups()
+        nodes[n] = {'L': l, 'R': r}
+
+
+def calc_steps(node):
+    steps = 0
+    while node[2] != 'Z':
+        node = nodes[node][ins[steps % len(ins)]]
+        steps += 1
+    return steps
+
+
+print(calc_steps('AAA'))
+print(math.lcm(*(calc_steps(n) for n in nodes.keys() if n[2] == 'A')))
