be506191d65676e223947542fcb48d1b76240186
Ondra <tethal@email.cz>
2023-12-09 09:37:10 +0100

    09

---
+++ 09.py
@@ -0,0 +1,15 @@
+import itertools
+
+
+def predict(src):
+    first = []
+    last = []
+    while any(bool(i) for i in src):
+        first.append(src[0])
+        last.append(src[-1])
+        src = [b - a for a, b in zip(src, src[1:])]
+    return sum(last), sum(a * b for a, b in zip(first, itertools.cycle((1, -1))))
+
+
+with open('09a.txt', 'rt') as f:
+    print(tuple(map(sum, zip(*(predict(tuple(map(int, line.split()))) for line in f)))))
