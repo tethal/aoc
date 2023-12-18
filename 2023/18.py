4ad2734c4e8f933b98f1e15dd0f43837ea53bac4
Ondra <tethal@email.cz>
2023-12-18 09:40:05 +0100

    18

---
+++ 2023/18.py
@@ -0,0 +1,29 @@
+
+DIRS = ((1, 0), (0, 1), (-1, 0), (0, -1))
+
+
+def parse(line):
+    c = line.split(' ')
+    x = int(c[2][2:-2], 16)
+    return 'RDLU'.index(c[0]), int(c[1]), x & 15, x >> 4
+
+
+def area(src):
+    vertices = []
+    p = (0, 0)
+    border = 0
+    for d, cnt, in src:
+        p = (p[0] + cnt * DIRS[d][0], p[1] + cnt * DIRS[d][1])
+        vertices.append(p)
+        border += cnt
+
+    vertices.append(vertices[0])
+    a = sum(vertices[i][1] * (vertices[i - 1][0] - vertices[i + 1][0]) for i in range(len(vertices) - 1))
+    return (border + a) // 2 + 1
+
+
+with open('ex/18.txt', 'rt') as f:
+    data = [parse(line) for line in f]
+
+print(area(map(lambda x: x[:2], data)))
+print(area(map(lambda x: x[2:], data)))
