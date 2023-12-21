2e3c1b5f421ac25ed69d38f8922152e90ee48f47
Ondra <tethal@email.cz>
2023-12-21 15:11:22 +0100

    21 alternative solution

--- 2023/21.py
+++ 2023/21.py
@@ -48,3 +48,29 @@ print(
     (tiles - 1) * get_count(w - 1, 0, w + sx - 1) +
     (tiles - 1) * get_count(0, 0, w + sx - 1)
 )
+
+
+def part2_alternative():
+    queue = {(sx, sy)}
+    for dist in range(1000):
+        if dist == sx:
+            y0 = len(queue)
+        if dist == sx + w:
+            y1 = len(queue)
+        if dist == sx + 2 * w:
+            y2 = len(queue)
+            c = y0
+            a = (y2 - 2 * y1 + y0) // 2
+            b = y1 - y0 - a
+            x = (26501365 - sx) // w
+            return a * x * x + b * x + c
+
+        nq = set()
+        for x, y in queue:
+            for nx, ny in (x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y):
+                if grid[ny % w][nx % w] in '.S':
+                    nq.add((nx, ny))
+        queue = nq
+
+
+print(part2_alternative())
